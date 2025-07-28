f3b2600a116e6606fa518c0b00ba1f769a731aa4
umaicode <park.seungchan99@gmail.com>
2025-07-28 10:49:38 +0900

    250725_FRI : api request

--- 250725/example2.py
+++ 250725/example2.py
@@ -5,15 +5,16 @@ def get_seoul_weather():
     # API KEY
     # API_KEY =
 
-    city_name = "Seoul, KR"
+    # city_name = "Seoul, KR"
     # city_name = "Tokyo, JP"
     # city_name = "New York, US"
 
     # url =
 
     # 캘빈 온도 출력
-    temperature = response["main"]["temp"]
-    print(f"캘빈 온도 : {temperature}")
+    # temperature = response["main"]["temp"]
+    # print(f"캘빈 온도 : {temperature}")
 
     # response = requests.get(url).json()
     # return response
+    pass
