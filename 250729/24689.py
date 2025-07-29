9258a48c1d4b49afb18835944b79e25ee1c2533c
umaicode <park.seungchan99@gmail.com>
2025-07-29 16:44:57 +0900

    24689 : set(), add() 사용

--- 250729/24689.py
+++ 250729/24689.py
@@ -1,16 +1,29 @@
+# numbers = list(map(int, input().split()))
+
+
+# def valid_numbers(numbers):
+#     lottery = set()
+#     for number in numbers:
+#         lottery.add(number)
+#     if len(lottery) != 6:
+#         return "INVALID"
+#     for num in lottery:
+#         if num < 1 or num > 45:
+#             return "INVALID"
+#     return "VALID"
+
+
+# print(valid_numbers(numbers))
+
+
 numbers = list(map(int, input().split()))
 
-
-def valid_numbers(numbers):
-    lottery = set()
-    for number in numbers:
-        lottery.add(number)
-    if len(lottery) != 6:
-        return "INVALID"
-    for num in lottery:
-        if num < 1 or num > 45:
-            return "INVALID"
-    return "VALID"
-
-
-print(valid_numbers(numbers))
+# lotto = {}    # 딕셔너리
+lotto = set()
+for num in numbers:
+    if 1 <= num <= 45:  # 2번 조건
+        lotto.add(num)
+if len(lotto) == 6:  # 1번 조건
+    print("VALID")
+else:
+    print("INVALID")
--- 250729/test.py
+++ 250729/test.py
@@ -1,9 +1,30 @@
-# import requests
-
-# requests.get(url)
-
-# 요청 방식
-# get, post
-
 # get과 setdefault의 차이
 # setdefault는 k의 key가 없으면 추가해준다.
+
+seen = {1, 6, 3, 5, 9, 999, -10, -3}
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+a = seen.pop()
+print(a)
+
+
+# Q) 이 소스코드를 실행할 때마다 결과가 다를까? 같을까?
+# A) 정수의 해시값은 일정하기 때문에 결과가 같다
+
+# Q) 정수의 해시값은 일정하다. pop을 했을 때 작은 값 먼저 제거, 반환?
+# A) X
+
+# Q) 세트의 요소에 문자가 있을 때도 실행할 때마다 결과가 같을까? 다를까?
+# A) 정수의 해시값은 일정하고, 문자의ㅏ 해시값은 일정하지 않다.
