208b7258c7b9f1d35f42f58d90e2642baa67f768
umaicode <park.seungchan99@gmail.com>
2025-07-29 16:16:19 +0900

    24683 : .items() key, value 가져와서 최대값 찾기

--- 250729/24683.py
+++ 250729/24683.py
@@ -1,11 +1,24 @@
+# scores = {"bogeom": 89, "sangho": 100, "IU": 78, "sori": 76, "hejun": 85}
+
+# scores_list = list(scores.items())
+
+# max_score = 0
+# for name in scores_list:
+#     if name[1] >= max_score:
+#         max_score = name[1]
+#         best_student = name[0]
+
+# print(best_student)
+
+
 scores = {"bogeom": 89, "sangho": 100, "IU": 78, "sori": 76, "hejun": 85}
 
-scores_list = list(scores.items())
+# 최대 점수 초기화
+max_v = 0
+best = ""
+for key, value in scores.items():
+    if value > max_v:
+        max_v = value  # 최대값 갱신 코드
+        best = key  # 최대값 갱신 되었을 때 학생
 
-max_score = 0
-for name in scores_list:
-    if name[1] >= max_score:
-        max_score = name[1]
-        best_student = name[0]
-
-print(best_student)
+print(best)
