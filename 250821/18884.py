7a3cb9e8429d46579746d6969778c01278349b49
umaicode <park.seungchan99@gmail.com>
2025-08-21 17:27:46 +0900

    18884 : 정중앙 대학교

--- 250821/18884.py
+++ 250821/18884.py
@@ -24,6 +24,7 @@ for _ in range(n):
         heapq.heappush(scores, s)
 """
 
+"""
 n = int(input())
 
 scores = [500]  # 초기값 500
@@ -37,3 +38,42 @@ for _ in range(n):
     length = len(scores)
     mid = scores[length // 2]
     print(mid)
+
+# 시간복잡도 O(N^2logN)
+"""
+import heapq
+
+max_heap = []  # 내림차순 중간값 보다 작은 값 (최대힙)
+min_heap = []  # 오름차순 중간값 보다 큰값 (최소힙)
+mid = 500
+
+
+def push(v):
+    if mid > v:  # 중간값보다 작으면 최대 힙에 추가
+        heapq.heappush(max_heap, -v)
+    else:  # 중간값보다 크거나 같으면 최소 힙에 추가
+        heapq.heappush(min_heap, v)
+
+
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    push(a)
+    push(b)
+
+    # 경우가 두 가지
+    # 1. 왼쪽이 더 많을 경우 (최대 힙의 크기가 더 클 경우)
+    # 2. 오른쪽이 더 많을 경우 (최소 힙의 크기가 더 클 경우)
+    if len(max_heap) > len(min_heap):
+        # 최대 힙이 많으니까 최소 힙에 넣기 (갯수 맞추기)
+        heapq.heappush(min_heap, mid)
+
+        # 최대 힙에서 가장 큰 값 꺼내서 새로운 중간값으로 설정
+        mid = -heapq.heappop(max_heap)
+
+    elif len(max_heap) < len(min_heap):
+        heapq.heappush(max_heap, -mid)
+        mid = heapq.heappop(min_heap)
+
+# 시간복잡도 O(NlogN)
