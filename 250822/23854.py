fbc62f09097f07dafa1f55325efdb3d131a60570
umaicode <park.seungchan99@gmail.com>
2025-08-22 17:09:16 +0900

    23854 : 이진트리의 중위순회

---
+++ 250822/23854.py
@@ -0,0 +1,22 @@
+bt = [0] * 100
+
+bt[1] = 9
+bt[2] = 4
+bt[3] = 12
+bt[4] = 3
+bt[5] = 6
+bt[7] = 15
+bt[14] = 13
+bt[15] = 17
+
+
+def dfs(now):
+    if bt[now] == 0:
+        return
+
+    dfs(now * 2)
+    print(bt[now], end=" ")
+    dfs(now * 2 + 1)
+
+
+dfs(1)
