dc693fee722649a3e8b506bfbef784682d38cb39
umaicode <park.seungchan99@gmail.com>
2025-09-02 14:50:08 +0900

    24024 : DFS 준비 2 그래프와 인접행렬

--- 250902/24024.py
+++ 250902/24024.py
@@ -1,3 +1,20 @@
+# 인접 행렬
+
+MAP = [
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+]
+
+num = int(input())  # 노드 값
+n = num - 1  # 노드 번호
+for i in range(5):
+    if MAP[n][i] == 0:
+        continue
+    print(i + 1)  # 노드 값
+
 """
 name = "12345"
 
@@ -16,20 +33,3 @@ for i in range(5):
         continue
     print(name[i])
 """
-
-# 인접 행렬
-
-MAP = [
-    [0, 1, 0, 0, 0],
-    [0, 0, 1, 1, 0],
-    [0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0],
-]
-
-num = int(input())  # 노드 값
-n = num - 1  # 노드 번호
-for i in range(5):
-    if MAP[n][i] == 0:
-        continue
-    print(i + 1)  # 노드 값
