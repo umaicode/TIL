86da60d998f356bb7cecf5f9cfe72bb955b6d62b
umaicode <park.seungchan99@gmail.com>
2025-09-16 16:31:49 +0900

    24213 : Union-Find 시작 2 같은 그룹인지 판단

--- 250915/24213.py
+++ 250915/24213.py
@@ -3,12 +3,14 @@ def make_set(n):
     ranks = [0] * (n + 1)
     return parents, ranks
 
+
 def find(x):
     if x == parents[x]:
         return x
 
     parents[x] = find(parents[x])
     return parents[x]
+
 
 def union(x, y):
     rep_x = find(x)
@@ -27,22 +29,25 @@ def union(x, y):
 
     return False
 
-n = 6
 
-arr = "ABCDE"
-parents, ranks = make_set(n)
 N = int(input())
-results = []
+is_cycle = False
+arr = "0ABCDE"
+parents, ranks = make_set(6)
+
 for _ in range(N):
     a, b = input().split()
     int_a = arr.index(a)
     int_b = arr.index(b)
-    results.append(union(int_a, int_b))
+    union(int_a, int_b)
 
-for result in results:
-    if not result:
-        print("O")
-    else:
-        print("X")
+node_1, node_2 = input().split()
+int_node_1 = arr.index(node_1)
+int_node_2 = arr.index(node_2)
+if union(int_node_1, int_node_2):
+    is_cycle = True
 
-
+if is_cycle:
+    print("O")
+else:
+    print("X")
